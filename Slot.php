ec3b1a7be12e8ffe8e30b00f771ccf1352b5649c
vadim248 <iwad2d@gmail.com>
2012-10-30 07:13:14 +0700

    class Symbol

---
+++ Slot.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Slot
+ *
+ * @author vadim24816
+ */
+class Slot {
+  
+}
+class WeightTable{
+  
+}
+
+?>
---
+++ Symbol.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Symbol
+ *
+ * @author vadim24816
+ */
+class Symbol {
+  
+}
+
+?>
