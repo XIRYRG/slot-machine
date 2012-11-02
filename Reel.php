acda576ffc7795e2b109891af1672c54cbbf40e4
vadim248 <iwad2d@gmail.com>
2012-11-02 23:04:19 +0700

    the algo

--- Reel.php
+++ Reel.php
@@ -13,18 +13,18 @@
 
 require_once 'dumpit.php';
 class Reel {
-  public $reel_line;
-  public function __construct() {
-    ;
+  public $reel_line,$name;
+  public function __construct($name) {
+    $this->name;
   }
-  public function get_new_randomly_generated_symbol(){
+  public function get_new_randomly_choosed_symbol(){
+    mt_srand();
+    //choose the symbol number
     $rand_num = mt_rand(0, 63);
-    return $rand_num;
+    $sym = $this->reel_line[$rand_num];
+    return $sym;
   }
 }
 
-$r1 = new Reel();
-echo $r1->get_new_randomly_generated_symbol();
-dump_it($r1);
-
+$r1 = new Reel('reel1');
 ?>
