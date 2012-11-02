25048e07e4c53bba244856ebe3733fb7b231d5d6
vadim248 <iwad2d@gmail.com>
2012-11-02 21:03:14 +0700

    reel lines

--- Reel.php
+++ Reel.php
@@ -10,10 +10,21 @@
  *
  * @author vadim24816
  */
+
+require_once 'dumpit.php';
 class Reel {
+  public $reel_line;
   public function __construct() {
     ;
   }
+  public function get_new_randomly_generated_symbol(){
+    $rand_num = mt_rand(0, 63);
+    return $rand_num;
+  }
 }
+
+$r1 = new Reel();
+echo $r1->get_new_randomly_generated_symbol();
+dump_it($r1);
 
 ?>
